fe569ce301b5ffb9651a46cf78889487b60c5d5c
haroon47 <haroonfazal@outlook.com>
2018-04-30 19:12:24 +0500

    Initial commit

---
+++ app/src/main/java/com/example/haroonpc/barcodescanner/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.haroonpc.barcodescanner;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Vibrator;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.hitomi.cmlibrary.CircleMenu;
+import com.hitomi.cmlibrary.OnMenuSelectedListener;
+
+import java.io.IOException;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+
+
+
+
+    }
+
+
+
+
+}
